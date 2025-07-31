da76c35b17448025007e18689e662aed74f95a31
Yan Lin <github@yanlincs.com>
2025-07-31 23:56:49 +0200

    Update firefox bookmark config

---
+++ config/firefox-bookmarks.nix
@@ -0,0 +1,94 @@
+{
+  force = true;
+  settings = [
+    {
+      name = "Toolbar";
+      toolbar = true;
+      bookmarks = [
+        {
+          name = "Links";
+          url = "https://link.nas.yanlincs.com";
+        }
+        {
+          name = "RSS";
+          url = "https://rss.nas.yanlincs.com";
+        }
+        {
+          name = "Overleaf";
+          url = "https://www.overleaf.com/project";
+        }
+        {
+          name = "Notion";
+          url = "https://www.notion.so";
+        }
+        {
+          name = "Paperless";
+          url = "https://paperless.nas.yanlincs.com";
+        }
+        {
+          name = "Nextcloud";
+          url = "https://cloud.nas.yanlincs.com";
+        }
+        {
+          name = "Productivity";
+          bookmarks = [
+            {
+              name = "Outlook";
+              url = "https://outlook.office.com/mail/";
+            }
+            {
+              name = "Gmail";
+              url = "https://mail.google.com/mail/u/0/#inbox";
+            }
+            {
+              name = "Teams";
+              url = "https://teams.microsoft.com/v2/";
+            }
+          ];
+        }
+        {
+          name = "My Home";
+          url = "https://home.nas.yanlincs.com/lovelace/0";
+        }
+        {
+          name = "Media";
+          bookmarks = [
+            {
+              name = "Immich";
+              url = "https://photo.nas.yanlincs.com/photos";
+            }
+            {
+              name = "Plex";
+              url = "https://plex.nas.yanlincs.com";
+            }
+            {
+              name = "Emby";
+              url = "https://emby.nas.yanlincs.com";
+            }
+            {
+              name = "Library";
+              bookmarks = [
+                {
+                  name = "Sonarr";
+                  url = "https://sonarr.nas.yanlincs.com";
+                }
+                {
+                  name = "Radarr";
+                  url = "https://radarr.nas.yanlincs.com";
+                }
+                {
+                  name = "qBittorrent";
+                  url = "https://qbit.nas.yanlincs.com";
+                }
+                {
+                  name = "MeTube";
+                  url = "https://metube.nas.yanlincs.com";
+                }
+              ];
+            }
+          ];
+        }
+      ];
+    }
+  ];
+}
