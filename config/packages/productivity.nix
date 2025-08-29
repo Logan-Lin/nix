1ac0a29a59899d058788ce135ad691902e61b99a
Yan Lin <github@yanlincs.com>
2025-08-29 23:19:50 +0200

    Rearrange home packages import

--- config/packages/productivity.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  home.packages = with pkgs; [
-    papis
-    keepassxc      # Password manager (Linux/Windows/macOS)
-    syncthing      # File synchronization (cross-platform)
-  ];
-}
