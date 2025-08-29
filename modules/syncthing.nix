96e2ef28ac4fd47592f79d3a369dfbaa99f9ee96
Yan Lin <github@yanlincs.com>
2025-08-29 07:37:29 +0200

    Auto load syncthing at startup

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,10 +1,15 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Enable Syncthing service
+  # Enable Syncthing service with auto-start on macOS
   services.syncthing = {
     enable = true;
     # Don't enable tray on macOS as it requires additional setup
     tray.enable = false;
+  };
+  
+  # Override the launchd agent to add RunAtLoad on macOS
+  launchd.agents.syncthing = lib.mkIf (pkgs.stdenv.isDarwin && config.services.syncthing.enable) {
+    config.RunAtLoad = true;
   };
 }
