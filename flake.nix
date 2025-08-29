6368fe594ac5fb77a2fbb9509a3b7c6b4addd5f1
Yan Lin <github@yanlincs.com>
2025-08-29 23:16:24 +0200

    Use host-specific home configuration

--- flake.nix
+++ flake.nix
@@ -46,39 +46,6 @@
       programs.zsh.enable = true;
     };
 
-    homeConfiguration = { pkgs, ... }: {
-      imports = [ 
-        nixvim.homeManagerModules.nixvim
-        ./modules/nvim.nix 
-        ./modules/tmux.nix 
-        ./modules/zsh.nix 
-        ./modules/ssh.nix
-        ./modules/git.nix
-        ./modules/lazygit.nix
-        ./modules/papis.nix
-        ./modules/termscp.nix
-        ./modules/rsync.nix
-        ./modules/btop.nix
-        ./modules/firefox.nix
-        ./modules/ghostty.nix
-        ./modules/syncthing.nix
-        ./config/fonts.nix
-        ./config/packages/common.nix
-        ./config/packages/darwin.nix
-        ./config/packages/dev.nix
-        ./config/packages/productivity.nix
-      ];
-
-      nixpkgs.config.allowUnfree = true;
-
-      home.username = "yanlin";
-      home.homeDirectory = "/Users/yanlin";
-      home.stateVersion = "24.05";
-
-      programs.home-manager.enable = true;
-
-
-    };
   in
   {
     darwinConfigurations."iMac" = nix-darwin.lib.darwinSystem {
@@ -97,10 +64,18 @@
       ];
     };
 
-    homeConfigurations.yanlin = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      modules = [ homeConfiguration ];
-      extraSpecialArgs = { inherit claude-code nixvim firefox-addons; };
+    homeConfigurations = {
+      "yanlin@iMac" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./hosts/darwin/iMac/home.nix ];
+        extraSpecialArgs = { inherit claude-code nixvim firefox-addons; };
+      };
+
+      "yanlin@MacBook-Air" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./hosts/darwin/MacBook-Air/home.nix ];
+        extraSpecialArgs = { inherit claude-code nixvim firefox-addons; };
+      };
     };
   };
 }
---
+++ hosts/darwin/iMac/home.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../../home/common.nix
+  ];
+
+  # iMac-specific home configuration
+  # Example: Different screen setup, desktop-specific tools, etc.
+  
+}
