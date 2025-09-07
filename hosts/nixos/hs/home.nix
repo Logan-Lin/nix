9a10ebbc33cf54b397293acfcc319e977fb3a6de
Yan Lin <github@yanlincs.com>
2025-09-07 18:04:48 +0200

    Add move-inbox alias

--- hosts/nixos/hs/home.nix
+++ hosts/nixos/hs/home.nix
@@ -9,6 +9,7 @@
   programs.zsh.shellAliases = {
       # Disk health monitoring
       smart-report = "sudo /home/yanlin/.config/nix/scripts/daily-smart-report.sh";
+      move-inbox = "cp -rl /mnt/storage/Media/downloads/.inbox/* /mnt/storage/Media/downloads/inbox && chown -R yanlin:users /mnt/storage/Media/downloads/inbox";
   };
   
 }
