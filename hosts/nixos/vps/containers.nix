5cc67b9347ff4bf081076e60fe52b196a17a245e
Yan Lin <github@yanlincs.com>
2025-09-08 21:23:59 +0200

    Add VPS reverse proxy services

--- hosts/nixos/vps/containers.nix
+++ hosts/nixos/vps/containers.nix
@@ -1,7 +1,65 @@
 { config, pkgs, lib, ... }:
 
+let
+  # Universal container configuration
+  commonUID = "1000";
+  commonGID = "100";
+  systemTZ = config.time.timeZone;
+in
 {
   # Container definitions for vps host
   virtualisation.oci-containers.containers = {
+    # Static web server for homepage
+    homepage = {
+      image = "docker.io/nginx:alpine";
+      
+      volumes = [
+        "/home/yanlin/www/homepage:/usr/share/nginx/html:ro"
+        "/home/yanlin/www/homepage-nginx.conf:/etc/nginx/conf.d/default.conf:ro"
+      ];
+
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.homepage.rule" = "Host(`www.yanlincs.com`)";
+        "traefik.http.routers.homepage.entrypoints" = "websecure";
+        "traefik.http.routers.homepage.tls" = "true";
+        "traefik.http.routers.homepage.tls.certresolver" = "cloudflare";
+        "traefik.http.routers.homepage.tls.domains[0].main" = "yanlincs.com";
+        "traefik.http.routers.homepage.tls.domains[0].sans[0]" = "*.yanlincs.com";
+        "traefik.http.services.homepage.loadbalancer.server.port" = "80";
+      };
+      
+      extraOptions = [
+        "--network=podman"
+      ];
+      
+      autoStart = true;
+    };
+
+    # Static web server for blog
+    blog = {
+      image = "docker.io/nginx:alpine";
+      
+      volumes = [
+        "/home/yanlin/www/blog:/usr/share/nginx/html:ro"
+        "/home/yanlin/www/blog-nginx.conf:/etc/nginx/conf.d/default.conf:ro"
+      ];
+
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.blog.rule" = "Host(`blog.yanlincs.com`)";
+        "traefik.http.routers.blog.entrypoints" = "websecure";
+        "traefik.http.routers.blog.tls" = "true";
+        "traefik.http.routers.blog.tls.certresolver" = "cloudflare";
+        "traefik.http.routers.blog.tls.domains[0].main" = "*.yanlincs.com";
+        "traefik.http.services.blog.loadbalancer.server.port" = "80";
+      };
+      
+      extraOptions = [
+        "--network=podman"
+      ];
+      
+      autoStart = true;
+    };
   };
 }
