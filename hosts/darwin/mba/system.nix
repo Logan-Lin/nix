2f8f98bd7dc6a55c5977382580ab98bdd4c8984d
Yan Lin <github@yanlincs.com>
2025-09-10 23:02:44 +0200

    Add darwin hosts to wireguard

--- hosts/darwin/mba/system.nix
+++ hosts/darwin/mba/system.nix
@@ -9,4 +9,20 @@
   imports = [
     ../system-default.nix
   ];
+
+  # WireGuard LaunchAgent for auto-start
+  launchd.user.agents.wireguard = {
+    serviceConfig = {
+      ProgramArguments = [
+        "/opt/homebrew/bin/wg-quick"
+        "up"
+        "/Users/yanlin/.config/nix/config/wireguard/mba.conf"
+      ];
+      RunAtLoad = true;
+      KeepAlive = false;
+      Label = "com.wireguard.mba";
+      StandardErrorPath = "/tmp/wireguard.err";
+      StandardOutPath = "/tmp/wireguard.out";
+    };
+  };
 }
