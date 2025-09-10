6ad3f273aa621e686b281b8d50ed3ccc0671796d
Yan Lin <github@yanlincs.com>
2025-09-11 01:11:35 +0200

    Disable caps lock key

--- hosts/darwin/system-default.nix
+++ hosts/darwin/system-default.nix
@@ -60,7 +60,7 @@
         "/usr/bin/hidutil"
         "property"
         "--set"
-        ''{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x700000039}]}''
+        ''{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0xFF00000003}]}''
       ];
       RunAtLoad = true;
       KeepAlive = false;
