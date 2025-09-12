bb38eee42fd95bb4b1f356e430c75c1dfca937f1
Yan Lin <github@yanlincs.com>
2025-09-12 09:09:54 +0200

    Add local proxy to webdav

--- hosts/nixos/hs/proxy.nix
+++ hosts/nixos/hs/proxy.nix
@@ -15,12 +15,35 @@
             }];
           };
         };
+
+        # WebDAV file server
+        files = {
+          rule = "Host(`files.${config.networking.hostName}.yanlincs.com`)";
+          entrypoints = "websecure";
+          service = "files";
+          tls = {
+            certResolver = "cloudflare";
+            domains = [{
+              main = "*.${config.networking.hostName}.yanlincs.com";
+            }];
+          };
+        };
       };
+
       services = {
         syncthing = {
           loadBalancer = {
             servers = [{
               url = "http://localhost:8384";
+            }];
+          };
+        };
+
+        # WebDAV file server
+        files = {
+          loadBalancer = {
+            servers = [{
+              url = "http://localhost:5009";
             }];
           };
         };
