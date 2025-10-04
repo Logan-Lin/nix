8572f31b5ee7b1a5626a69bff8feb7db26571ee7
Yan Lin <github@yanlincs.com>
2025-10-04 21:15:48 +0200

    Add custom search in vimium

--- config/firefox/search.nix
+++ config/firefox/search.nix
@@ -3,45 +3,6 @@
   default = "ddg";
   
   engines = {
-    "ddg" = {
-      urls = [{
-        template = "https://duckduckgo.com/?q={searchTerms}";
-      }];
-      icon = "https://duckduckgo.com/favicon.ico";
-      definedAliases = [ "@ddg" ];
-    };
-    
-    "Nix Packages" = {
-      urls = [{
-        template = "https://search.nixos.org/packages";
-        params = [
-          { name = "channel"; value = "unstable"; }
-          { name = "query"; value = "{searchTerms}"; }
-        ];
-      }];
-      definedAliases = [ "@nixpkg" ];
-    };
-
-    "Linkding" = {
-      urls = [{
-        template = "https://link.nas.yanlincs.com/bookmarks";
-        params = [
-          { name = "q"; value = "{searchTerms}"; }
-        ];
-      }];
-      definedAliases = [ "@link" ];
-    };
-
-    "google" = {
-      urls = [{
-        template = "https://www.google.com/search";
-        params = [
-          { name = "q"; value = "{searchTerms}"; }
-        ];
-      }];
-      definedAliases = [ "@g" ];
-    };
-    
     # Hide unwanted default search engines
     "google".metaData.hidden = true;
     "bing".metaData.hidden = true;
