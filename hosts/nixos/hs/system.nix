38d1c6aa7f39cbbbbb5140965402ee6e2c6d27be
Yan Lin <github@yanlincs.com>
2025-10-10 09:34:55 +0200

    revise login display

--- hosts/nixos/hs/system.nix
+++ hosts/nixos/hs/system.nix
@@ -224,9 +224,9 @@
     smartDrives = {
       "/dev/disk/by-id/ata-ZHITAI_SC001_XT_1000GB_ZTB401TAB244431J4R" = "ZFS_Mirror_1";
       "/dev/disk/by-id/ata-ZHITAI_SC001_XT_1000GB_ZTB401TAB244431KEG" = "ZFS_Mirror_2";
-      "/dev/disk/by-id/ata-HGST_HUH721212ALE604_5PK2N4GB" = "Data_Drive_1_12TB";
-      "/dev/disk/by-id/ata-HGST_HUH721212ALE604_5PJ7Z3LE" = "Data_Drive_2_12TB";
-      "/dev/disk/by-id/ata-ST16000NM000J-2TW103_WRS0F8BE" = "Parity_Drive_16TB";
+      "/dev/disk/by-id/ata-HGST_HUH721212ALE604_5PK2N4GB" = "Data_1_12TB";
+      "/dev/disk/by-id/ata-HGST_HUH721212ALE604_5PJ7Z3LE" = "Data_2_12TB";
+      "/dev/disk/by-id/ata-ST16000NM000J-2TW103_WRS0F8BE" = "Parity_16TB";
     };
     showDiskUsage = true;
     diskUsagePaths = [ "/" "/mnt/storage" "/mnt/parity" ];
