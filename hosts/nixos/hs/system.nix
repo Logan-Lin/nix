a61642ce0b1fe4dd0e93e989402b1bf1776d49d3
Yan Lin <github@yanlincs.com>
2025-10-11 19:21:35 +0200

    Add snapraid login display

--- hosts/nixos/hs/system.nix
+++ hosts/nixos/hs/system.nix
@@ -230,6 +230,7 @@
     };
     showDiskUsage = true;
     diskUsagePaths = [ "/" "/mnt/storage" "/mnt/parity" ];
+    showSnapraidStatus = true;
     showContainerUpdater = true;
     showBorgStatus = true;
   };
