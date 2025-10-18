e5273c08f67e2efccd4dbc25dee02e60f0f16e93
Yan Lin <github@yanlincs.com>
2025-10-18 14:44:56 +0200

    remove ghostty from thinkpad

--- hosts/nixos/thinkpad/home.nix
+++ hosts/nixos/thinkpad/home.nix
@@ -8,17 +8,8 @@
   imports = [
     ../home-default.nix
     ../../../modules/syncthing.nix
-    ../../../modules/ghostty.nix
     ../../../modules/tex.nix
   ];
-
-  # Enable Ghostty terminal with NixOS package
-  programs.ghostty-custom = {
-    enable = true;
-    package = pkgs.ghostty;  # Install via nix on NixOS
-    fontSize = 11;
-    windowMode = "maximized";
-  };
 
   # Any ThinkPad-specific home configurations can be added here
   # For example, laptop-specific aliases or scripts
