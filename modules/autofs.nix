b12481d7cb4bdfa05e0b5e4453595dd7fe9488fd
Yan Lin <github@yanlincs.com>
2025-10-25 15:35:16 +0200

    add LAN prefer to nfs

--- modules/autofs.nix
+++ modules/autofs.nix
@@ -12,7 +12,7 @@ in
 
     remoteHost = mkOption {
       type = types.str;
-      description = "Remote NFS server hostname or IP";
+      description = "Primary remote NFS server hostname or IP";
     };
 
     remotePath = mkOption {
@@ -24,15 +24,28 @@ in
       type = types.str;
       description = "Local mount point";
     };
+
+    replicas = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Replica server hostnames or IPs for failover (in order of preference)";
+    };
   };
 
   config = mkIf cfg.enable {
     services.autofs = {
       enable = true;
       timeout = 300;
-      autoMaster = ''
-        ${cfg.mountPoint} -fstype=nfs4,rw,soft,intr,noatime ${cfg.remoteHost}:${cfg.remotePath}
-      '';
+      autoMaster =
+        let
+          # Build server list: primary host followed by replicas
+          allHosts = [ cfg.remoteHost ] ++ cfg.replicas;
+          # Format as "host1:/path host2:/path host3:/path"
+          locations = concatStringsSep " " (map (host: "${host}:${cfg.remotePath}") allHosts);
+        in
+        ''
+          ${cfg.mountPoint} -fstype=nfs4,rw,soft,intr,noatime ${locations}
+        '';
     };
 
     systemd.tmpfiles.rules = [
