8f3cab555e187d6200058552d291f38cde7c0130
Yan Lin <github@yanlincs.com>
2025-11-02 21:18:03 +0100

    add a few modules to deck

--- hosts/nixos/deck/home.nix
+++ hosts/nixos/deck/home.nix
@@ -7,6 +7,16 @@
   # Import the common NixOS home configuration
   imports = [
     ../home-default.nix
+    ../../../modules/syncthing.nix
+    ../../../modules/firefox.nix
+    ../../../modules/ghostty.nix
     ../../../modules/gnome.nix
+  ];
+
+  # ThinkPad-specific applications
+  home.packages = with pkgs; [
+    obsidian
+    keepassxc
+    vlc
   ];
 }
