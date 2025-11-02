81ea82d1d2093abf182991c99c43320ded8963a6
Yan Lin <github@yanlincs.com>
2025-11-02 18:42:37 +0100

    remove some ytb and bilibili channels

--- hosts/nixos/hs/home.nix
+++ hosts/nixos/hs/home.nix
@@ -29,24 +29,10 @@
       "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@Danny-Gonzalez/videos'"
       "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@rejectconvenience/videos'"
       "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@StuffMadeHere/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@AdamSomething/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@_gerg/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@Yeah_Jaron/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@WolfgangsChannel/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@ThatChiefGuy/videos'"
       "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@DankPods/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@NuclearNotebook/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@thespiffingbrit/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@DoshDoshington/videos'"
       "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@ScottTheWoz/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@JeffGeerling/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@optimumtech/videos'"
-      "dlv youtube -n 3 -r 1 --min 1 --max 180 'https://www.youtube.com/@Wato-1876/videos'"
       "dlv bilibili -n 3 -r 1 --title '摸鱼切片' 'https://space.bilibili.com/15810/upload/video'"  # Mr.Quin
       "dlv bilibili -n 3 -r 1 --title 'PGN' 'https://space.bilibili.com/8012266/upload/video'"  # PMM_LORD
-      "dlv bilibili -n 3 -r 1 'https://space.bilibili.com/213741/upload/video'"  # 中二电波台
-      "dlv bilibili -n 3 -r 1 'https://space.bilibili.com/1629347259/upload/video'"  # 红警HBK08
-      "dlv bilibili -n 3 -r 1 'https://space.bilibili.com/483246073/upload/video'"  # 红警魔鬼蓝天
     ];
   };
   
