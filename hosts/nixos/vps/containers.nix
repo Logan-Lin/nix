5af106054ed42f6251ab0bd1cd216b0986b0af33
Yan Lin <github@yanlincs.com>
2025-11-19 13:15:40 +0100

    add back oc-scheduler container

--- hosts/nixos/vps/containers.nix
+++ hosts/nixos/vps/containers.nix
@@ -9,5 +9,18 @@ in
 {
   # Container definitions for vps host
   virtualisation.oci-containers.containers = {
+
+    # OC Backend Scheduler
+    oc-scheduler = {
+      image = "localhost/oc-scheduler:v1";
+
+      extraOptions = [
+        "--network=podman"
+        "--security-opt=no-new-privileges:true"
+      ];
+      
+      autoStart = true;
+    };
+
   };
 }
