ddc822f8d61fe08f08b0f6720030f0f29ff6c48f
Yan Lin <github@yanlincs.com>
2025-11-22 10:30:33 +0100

    remove *arr stack

--- hosts/nixos/vps/proxy.nix
+++ hosts/nixos/vps/proxy.nix
@@ -18,30 +18,6 @@
           };
         };
 
-        # Sonarr TV show management
-        sonarr = {
-          rule = "Host(`sonarr.yanlincs.com`)";
-          service = "sonarr";
-          tls = {
-            certResolver = "cloudflare";
-            domains = [{
-              main = "*.yanlincs.com";
-            }];
-          };
-        };
-
-        # Radarr movie management
-        radarr = {
-          rule = "Host(`radarr.yanlincs.com`)";
-          service = "radarr";
-          tls = {
-            certResolver = "cloudflare";
-            domains = [{
-              main = "*.yanlincs.com";
-            }];
-          };
-        };
-
         # Jellyfin Media Server
         jellyfin = {
           rule = "Host(`jellyfin.yanlincs.com`)";
@@ -111,24 +87,6 @@
           loadBalancer = {
             servers = [{
               url = "http://10.2.2.20:5000";
-            }];
-          };
-        };
-
-        # Sonarr backend (via WireGuard)
-        sonarr = {
-          loadBalancer = {
-            servers = [{
-              url = "http://10.2.2.20:5003";
-            }];
-          };
-        };
-
-        # Radarr backend (via WireGuard)
-        radarr = {
-          loadBalancer = {
-            servers = [{
-              url = "http://10.2.2.20:5004";
             }];
           };
         };
