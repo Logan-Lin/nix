a14af7dbb48b0bcf0ca16352d46dc0c5cd0493dd
Yan Lin <github@yanlincs.com>
2025-11-23 15:35:10 +0100

    adjust thinkpad CPU scaling

--- hosts/nixos/thinkpad/system.nix
+++ hosts/nixos/thinkpad/system.nix
@@ -115,7 +115,7 @@
     enable = true;
     settings = {
       # CPU power management
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
