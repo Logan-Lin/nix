5a75588042ad4aaa879582deb9b9659ad217a7b4
Yan Lin <github@yanlincs.com>
2025-12-05 23:07:12 +0100

    use rsync to backup files

--- hosts/nixos/hs/home.nix
+++ hosts/nixos/hs/home.nix
@@ -16,6 +16,17 @@
       "rsync -avP aicloud:~/ /mnt/storage/Backup/aicloud/ --exclude='/.*'"
     ];
   };
+
+  services.scheduled-commands.backup-to-thinkpad = {
+    enable = true;
+    description = "Backup files to thinkpad";
+    interval = "*-*-* 00:00:00";
+    commands = [
+      "rsync-backup /mnt/storage/appbulk/immich/library/admin thinkpad:~/Backup/photos/immich-library"
+      "rsync-backup /mnt/storage/Media/DCIM thinkpad:~/Backup/photos/DCIM"
+      "rsync-backup /mnt/storage/Media/nsfw thinkpad:~/Backup/nsfw"
+    ];
+  };
   
   home.packages = with pkgs; [
   ];
