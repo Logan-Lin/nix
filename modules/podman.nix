eece71f15d032c0e7ae8429f8bf1c5a3cc055237
Yan Lin <github@yanlincs.com>
2025-12-26 03:44:56 +0100

    minor

--- modules/podman.nix
+++ modules/podman.nix
@@ -44,14 +44,6 @@ let
     done
 
     echo "Container update scan complete!"
-
-    # Clean up dangling images
-    echo "Cleaning up dangling images..."
-    if podman image prune -af; then
-      echo "Image cleanup complete!"
-    else
-      echo "Warning: Image cleanup failed"
-    fi
   '';
 in
 {
