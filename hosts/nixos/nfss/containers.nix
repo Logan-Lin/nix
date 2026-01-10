37738f03a2d20adb7b46fe23fd87aec70e7574e3
Yan Lin <github@yanlincs.com>
2026-01-10 06:41:58 +0100

    add podman to nfss

---
+++ hosts/nixos/nfss/containers.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+let
+  # Universal container configuration
+  commonUID = "1000";
+  commonGID = "100";
+  systemTZ = config.time.timeZone;
+in
+{
+  # Container definitions for hs host
+  virtualisation.oci-containers.containers = {
+  };
+}
