e4cbc55ff8cae24daf235d5f24125d16fa409ec0
Yan Lin <github@yanlincs.com>
2026-01-12 11:26:42 +0100

    add video2av1 function

--- modules/transcode.nix
+++ modules/transcode.nix
@@ -16,5 +16,20 @@
         fi
       done
     }
+
+    function video2av1() {
+      local dir="''${1:-.}"
+      for f in "$dir"/**/*.(mp4|mkv|avi); do
+        if [[ -f "$f" ]]; then
+          local outfile="./transcode/''${f%.*}.mkv"
+          mkdir -p "$(dirname "$outfile")"
+          ffmpeg -hwaccel qsv -i "$f" \
+            -c:v av1_qsv -global_quality 30 \
+            -vf "scale='min(720,iw)':-2" \
+            -c:a copy \
+            "$outfile"
+        fi
+      done
+    }
   '';
 }
