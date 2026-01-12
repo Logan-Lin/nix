4ecef9215916d7274685b13b48f2156bab8a288a
Yan Lin <github@yanlincs.com>
2026-01-12 17:07:29 +0100

    make flac2aac parallel

--- modules/transcode.nix
+++ modules/transcode.nix
@@ -8,13 +8,12 @@
   programs.zsh.initContent = ''
     function flac2aac() {
       local dir="''${1:-.}"
-      for f in "$dir"/**/*.flac; do
-        if [[ -f "$f" ]]; then
-          local outfile="./transcode/''${f%.flac}.m4a"
-          mkdir -p "$(dirname "$outfile")"
-          ffmpeg -i "$f" -vn -c:a aac -b:a 256k -movflags +faststart "$outfile"
-        fi
-      done
+      find "$dir" -name '*.flac' -type f -print0 | xargs -0 -P4 -n1 sh -c '
+        f="$1"
+        outfile="./transcode/''${f%.flac}.m4a"
+        mkdir -p "$(dirname "$outfile")"
+        ffmpeg -i "$f" -vn -c:a aac -b:a 256k -movflags +faststart "$outfile"
+      ' _
     }
 
     function video2av1() {
