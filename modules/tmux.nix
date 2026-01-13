01c728ec173fc484cd3d5bf06aef5310787090f9
Yan Lin <github@yanlincs.com>
2026-01-13 14:32:30 +0100

    fix nvim restore

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -15,6 +15,7 @@
         extraConfig = ''
           set -g @resurrect-strategy-nvim 'session'
           set -g @resurrect-processes 'nvim lazygit claude ssh'
+          set -g @resurrect-hook-post-save-all 'target=$(readlink -f ~/.tmux/resurrect/last); sed -i "" "s|/nix/store/[^/]*/bin/nvim --cmd .*|nvim|g" "$target"'
         '';
       }
       {
