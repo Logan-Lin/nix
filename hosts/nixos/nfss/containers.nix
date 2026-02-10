ee24f2848b005c12888accc36763374c18b77ff3
Yan Lin <github@yanlincs.com>
2026-02-10 14:34:17 +0100

    remove mongodb from nfss

--- hosts/nixos/nfss/containers.nix
+++ hosts/nixos/nfss/containers.nix
@@ -10,15 +10,5 @@ in
   # Container definitions for nfss host
   virtualisation.oci-containers.containers = {
 
-    mongodb = {
-      image = "docker.io/mongo:7";
-      volumes = [ "/var/lib/mongodb:/data/db" ];
-      environment = { TZ = systemTZ; };
-      environmentFiles = [ "/etc/mongodb-env" ];
-      ports = [ "27017:27017" ];
-      extraOptions = [ "--network=podman" ];
-      autoStart = true;
-    };
-
   };
 }
