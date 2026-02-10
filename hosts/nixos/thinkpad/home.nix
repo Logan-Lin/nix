0e4f487b239b189a7f7ee792a8a44b5b1c435862
Yan Lin <github@yanlincs.com>
2026-02-10 18:05:35 +0100

    add dcim consume schedule

--- hosts/nixos/thinkpad/home.nix
+++ hosts/nixos/thinkpad/home.nix
@@ -5,5 +5,16 @@
     ../home-default.nix
     ../../../modules/syncthing.nix
     ../../../modules/media-process.nix
+    ../../../modules/schedule.nix
   ];
+
+  services.scheduled-commands.dcim-consume = {
+    enable = true;
+    description = "Move files in dcim consume folder to DCIM";
+    interval = "*-*-* *:15:00";
+    commands = [
+      "photo-move -d /home/yanlin/Media/dcim-consume /home/yanlin/DCIM"
+    ];
+  };
+
 }
