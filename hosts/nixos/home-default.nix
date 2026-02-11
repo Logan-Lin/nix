8cb3aab57ba8ac82cba68ed387565b99a319e106
Yan Lin <github@yanlincs.com>
2026-02-11 20:45:12 +0100

    remove tunnel functions

--- hosts/nixos/home-default.nix
+++ hosts/nixos/home-default.nix
@@ -29,11 +29,6 @@
   # nixOS-specific alias
   programs.zsh.shellAliases = {
       oss = "sudo nixos-rebuild switch --flake ~/.config/nix#$(hostname)";
-
-      # Network monitoring aliases (no sudo needed - NixOS module handles permissions)
-      bw = "bandwhich";
-      bw-raw = "bandwhich --raw";
-      bw-dns = "bandwhich --show-dns";
   };
 
   home.packages = with pkgs; [
