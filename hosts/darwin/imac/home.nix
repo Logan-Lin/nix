7dfe9455b579d16c2de98f348d266da491e1598f
Yan Lin <github@yanlincs.com>
2026-02-11 20:14:57 +0100

    minor

--- hosts/darwin/imac/home.nix
+++ hosts/darwin/imac/home.nix
@@ -4,5 +4,4 @@
   imports = [
     ../home-default.nix
   ];
-
 }
--- hosts/darwin/macbook/system.nix
+++ hosts/darwin/macbook/system.nix
@@ -1,11 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # MacBook-specific configuration
   networking.computerName = "macbook";
   networking.hostName = "macbook";
 
-  # Import common Darwin configuration
   imports = [
     ../system-default.nix
   ];
