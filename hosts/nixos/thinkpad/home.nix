d6429617adf016bb621f4c86f020bb4a523eceae
Yan Lin <github@yanlincs.com>
2026-02-14 08:27:08 +0100

    add aicloud backup schedule

--- hosts/nixos/thinkpad/home.nix
+++ hosts/nixos/thinkpad/home.nix
@@ -5,6 +5,7 @@
     ../home-default.nix
     ../../../modules/syncthing.nix
     ../../../modules/media/tool.nix
+    ../../../modules/schedule.nix
   ];
 
   syncthing-custom.folders = {
@@ -12,6 +13,17 @@
     Documents.maxAgeDays = 30;
     Media.maxAgeDays = 7;
     Archive.maxAgeDays = 30;
+  };
+  
+  services.scheduled-commands.aicloud-backup = {
+    enable = true;
+    description = "Backup outputs files on aicloud";
+    interval = "*-*-* *:10:00";
+    commands = [
+      "rsync -avhP aicloud.lan:~/xrd-cond-glass-gen/{outputs,checkpoints,stdout} ~/run/xrd-cond-glass-gen"
+      "rsync -avhP aicloud.lan:~/charge-bal/{outputs,checkpoints,stdout} ~/run/charge-bal"
+      "rsync -avhP aicloud.lan:~/sr-gen-traj/{outputs,checkpoints,stdout} ~/run/sr-gen-traj"
+    ];
   };
 
 }
