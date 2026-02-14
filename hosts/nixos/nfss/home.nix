d61d951de3428ca705c3c6bc0c1593ebd8ed23e1
Yan Lin <github@yanlincs.com>
2026-02-14 17:09:54 +0100

    remove DCIM from syncthing

--- hosts/nixos/nfss/home.nix
+++ hosts/nixos/nfss/home.nix
@@ -14,7 +14,6 @@
     Media = { enable = true; maxAgeDays = 7; };
     Archive = { enable = true; maxAgeDays = 30; };
     Consume = { enable = true; maxAgeDays = 7; };
-    DCIM = { enable = true; maxAgeDays = 7; path = "/mnt/essd/DCIM"; };
   };
 
   services.scheduled-commands.dcim-consume = {
