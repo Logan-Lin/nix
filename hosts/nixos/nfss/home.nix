6c915b5a0964fdca4c0538b7c94eaa9d81017abd
Yan Lin <github@yanlincs.com>
2026-02-14 07:39:02 +0100

    move the photo-move schedule to nfss

--- hosts/nixos/nfss/home.nix
+++ hosts/nixos/nfss/home.nix
@@ -5,6 +5,7 @@
     ../home-default.nix
     ../../../modules/syncthing.nix
     ../../../modules/media/tool.nix
+    ../../../modules/schedule.nix
   ];
 
   syncthing-custom.folders = {
@@ -13,4 +14,14 @@
     Media.maxAgeDays = 7;
     Archive.maxAgeDays = 30;
   };
+
+  services.scheduled-commands.dcim-consume = {
+    enable = true;
+    description = "Move files in dcim consume folder to DCIM";
+    interval = "*-*-* *:00/15:00";
+    commands = [
+      "photo-move -d /home/yanlin/Documents/consume/dcim /mnt/essd/DCIM"
+    ];
+  };
+
 }
