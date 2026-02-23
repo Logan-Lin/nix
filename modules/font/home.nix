a2cd805cac5c429086b28da3f60d6570df16b26c
Yan Lin <git@yanlincs.com>
2026-02-23 20:50:00 +0100

    small fix

--- modules/font/home.nix
+++ modules/font/home.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 
 let
-  # Custom fonts from local files
   customFonts = pkgs.stdenvNoCC.mkDerivation {
     name = "custom-fonts";
     src = ./fonts;
-
     installPhase = ''
       mkdir -p $out/share/fonts
       find $src -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.ttc" -o -iname "*.woff" -o -iname "*.woff2" \) -exec cp {} $out/share/fonts/ \;
@@ -13,37 +11,26 @@ let
   };
 in
 {
-  # Font packages
   home.packages = with pkgs; [
-    # DejaVu font family
     dejavu_fonts
-
-    # Nerd Fonts with programming ligatures and icon support
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
-
-    # Ubuntu font family
     ubuntu-classic
-
-    # CJK fonts for proper Chinese/Japanese/Korean character display
-    noto-fonts-cjk-sans     # Comprehensive CJK support
-    noto-fonts-cjk-serif    # Serif CJK text support
-    source-han-sans         # High-quality CJK font alternative
-
-    # Traditional Chinese font styles
-    arphic-ukai             # KaiTi style (楷体) - brush stroke style
-    arphic-uming            # MingTi/Song style (宋体) - serif style
-    wqy_zenhei              # WenQuanYi Zen Hei - popular sans-serif with good coverage
-
-    # Japanese fonts
-    ipafont                        # IPA Gothic & Mincho - professional Japanese fonts (ゴシック体・明朝体)
-    mplus-outline-fonts.githubRelease  # M+ Family - modern Japanese font with multiple weights
-    kochi-substitute               # Kochi fonts - legacy MS Gothic/Mincho replacement
-
-    # Custom fonts from config/fonts directory
+    noto-fonts
+    noto-fonts-color-emoji
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    source-sans-pro
+    fira
+    source-han-sans
+    arphic-ukai
+    arphic-uming
+    wqy_zenhei
+    ipafont
+    mplus-outline-fonts.githubRelease
+    kochi-substitute
     customFonts
   ];
 
-  # Enable font configuration
   fonts.fontconfig.enable = true;
 }
