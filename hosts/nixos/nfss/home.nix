746966226da2afc919479af8c4708de0579bd93a
Yan Lin <git@yanlincs.com>
2026-04-01 20:27:44 +0200

    add yt-dlp module

--- hosts/nixos/nfss/home.nix
+++ hosts/nixos/nfss/home.nix
@@ -5,6 +5,7 @@
     ../home-default.nix
     ../../../modules/syncthing.nix
     ../../../modules/media/tool.nix
+    ../../../modules/media/yt-dlp.nix
     ../../../modules/claude-code.nix
   ];
 
@@ -13,6 +14,11 @@
     Documents = { enable = true; maxAgeDays = 30; };
     Media = { enable = true; maxAgeDays = 7; };
     Archive = { enable = true; maxAgeDays = 30; };
+  };
+
+  programs.yt-dlp-custom = {
+    enable = true;
+    downloadDir = "~/Media";
   };
 
 }
