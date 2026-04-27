5c6d6c4e2cdd327c282e8e515521b265ba554b8d
Yan Lin <git@yanlincs.com>
2026-04-27 20:35:40 +0200

    switch aicloud backup to use remote hostname

--- hosts/nixos/thinkpad/home.nix
+++ hosts/nixos/thinkpad/home.nix
@@ -26,9 +26,9 @@
     description = "Backup outputs files on aicloud";
     interval = "*-*-* *:10:00";
     commands = [
-      "rsync -avhP aicloud.lan:~/xrd-cond-glass-gen/{outputs,checkpoints,stdout} ~/run/xrd-cond-glass-gen"
-      "rsync -avhP aicloud.lan:~/charge-bal/{outputs,checkpoints,stdout} ~/run/charge-bal"
-      "rsync -avhP aicloud.lan:~/sr-gen-traj/{outputs,checkpoints,stdout} ~/run/sr-gen-traj"
+      "rsync -avhP aicloud:~/xrd-cond-glass-gen/{outputs,checkpoints,stdout} ~/run/xrd-cond-glass-gen"
+      "rsync -avhP aicloud:~/charge-bal/{outputs,checkpoints,stdout} ~/run/charge-bal"
+      "rsync -avhP aicloud:~/sr-gen-traj/{outputs,checkpoints,stdout} ~/run/sr-gen-traj"
     ];
   };
 
