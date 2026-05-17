0b1ceb95e69630f8867130211698f933d3cf54ac
Yan Lin <git@yanlincs.com>
2026-05-17 15:26:46 +0200

    declare hyprland system fonts

--- modules/hyprland/system.nix
+++ modules/hyprland/system.nix
@@ -41,6 +41,13 @@ in
       config.common.default = "hyprland;gtk";
     };
 
+    fonts.fontconfig.defaultFonts = {
+      sansSerif = [ "Noto Sans" "Noto Sans CJK SC" "Noto Sans CJK TC" "Noto Sans CJK JP" "Noto Sans CJK KR" ];
+      serif = [ "Noto Serif" "Noto Serif CJK SC" "Noto Serif CJK TC" "Noto Serif CJK JP" "Noto Serif CJK KR" ];
+      monospace = [ "JetBrainsMono Nerd Font Mono" "Noto Sans Mono CJK SC" "Noto Sans Mono CJK TC" "Noto Sans Mono CJK JP" "Noto Sans Mono CJK KR" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+
     programs.dconf.enable = true;
     programs.xfconf.enable = true;
 
