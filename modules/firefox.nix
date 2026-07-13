9d42eec318716e7f3ea2af9f211534e2410f469a
Yan Lin <git@yanlincs.com>
2026-07-13 08:44:58 +0200

    feat: add tailscale admin console bookmark to firefox

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -80,6 +80,7 @@ in
                   { name = "ntfy"; url = "https://ntfy.sh/yanlincs-homelab"; }
                   { name = "Deluge"; url = "https://deluge.yanlincs.com/"; }
                   { name = "Syncthing"; url = "http://127.0.0.1:8384/"; }
+                  { name = "Tailscale"; url = "https://login.tailscale.com/admin/machines"; }
                 ];
               }
               {
